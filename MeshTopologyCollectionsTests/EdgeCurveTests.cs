b905fd1264ff86ad7c00d070771617a7eb6d5842
Mustafa SENTÜRK <deniz_kurdu_1@hotmail.com>
2024-02-22 09:20:22 +0300

    Test - tests are improved.

---
+++ MeshTopologyCollectionsTests/EdgeCurveTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MeshTopologyCollections;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeshTopologyCollections.Tests
+{
+    [TestClass()]
+    public class EdgeCurveTests
+    {
+        [TestMethod()]
+        public void EdgeCurveTest()
+        {
+            var segments = new List<Tuple<VertexIndex, VertexIndex>>();
+            segments.Add(new Tuple<VertexIndex, VertexIndex>(0, 1));
+            segments.Add(new Tuple<VertexIndex, VertexIndex>(1, 2));
+            segments.Add(new Tuple<VertexIndex, VertexIndex>(2, 3));
+            var edgeCurve = new EdgeCurve(new TopologyVertexList(3));
+            bool res = edgeCurve.Add(segments);
+            Assert.Fail();
+            Assert.Equals(res, true);
+        }
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MeshTopologyCollections;
+
+Console.WriteLine("Hello, World!");
+
+var segments = new List<Tuple<VertexIndex, VertexIndex>>();
+segments.Add(new Tuple<VertexIndex, VertexIndex>(0, 1));
+segments.Add(new Tuple<VertexIndex, VertexIndex>(1, 2));
+var edgeCurve = new EdgeCurve(new TopologyVertexList(3));
+bool res = edgeCurve.Add(segments);
+
+if (!res)
+{
+    Console.WriteLine(res);
+}
